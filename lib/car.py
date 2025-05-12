4be9d9789f9f638f1da3136db3ee8a7ae8c7471a
mohamed abass tawana <aminabass987@gmail.com>
2025-05-12 14:50:17 +0300

    1st commit

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,12 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+        
+        
+        
+        
+
+    
